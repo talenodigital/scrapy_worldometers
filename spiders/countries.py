8804b42a4af6e688e51365b6c1022e321a952384
talenodigital <josemari.taleno@gmail.com>
2022-11-28 13:18:15 +0800

    Update countries.py
    First commit. Added s in http

--- spiders/countries.py
+++ spiders/countries.py
@@ -4,7 +4,7 @@ import scrapy
 class CountriesSpider(scrapy.Spider):
     name = 'countries'
     allowed_domains = ['www.worldometers.info']
-    start_urls = ['http://www.worldometers.info/world-population/population-by-country/']
+    start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
 
     def parse(self, response):
         pass
